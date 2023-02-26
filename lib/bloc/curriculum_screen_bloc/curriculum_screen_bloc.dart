d156a5341ed0327d30e6d1faa7e9d347c93f307e
Erik <erikmompean@gmail.com>
2023-02-26 15:15:43 +0100

    mouse scroll commit

--- lib/bloc/curriculum_screen_bloc/curriculum_screen_bloc.dart
+++ lib/bloc/curriculum_screen_bloc/curriculum_screen_bloc.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class CurriculumScreenBloc extends Bloc<CurriculumEvent, CurriculumState> {
+  double lastScroll = 0;
   final ScrollController _scrollController = ScrollController();
 
   CurriculumScreenBloc() : super(CurriculumStateInit()) {
@@ -20,9 +21,23 @@ class CurriculumScreenBloc extends Bloc<CurriculumEvent, CurriculumState> {
       CurriculumScrollUpdatedEvent event, Emitter<CurriculumState> emit) async {
     double angle = event.scrollPosition / 8;
 
+    if ((event.scrollPosition - lastScroll) > 50) {
+      // int loops = (event.scrollPosition - lastScroll).round();
+
+      for (var i = lastScroll; i < event.scrollPosition; i++) {
+        angle = i / 8;
+        await Future.delayed(Duration(milliseconds: 1));
+        if (angle > 30) {
+          angle = 30;
+        }
+        emit(CurriculumStateIdle(_scrollController, angle));
+      }
+    }
     if (angle > 30) {
       angle = 30;
     }
     emit(CurriculumStateIdle(_scrollController, angle));
+
+    lastScroll = event.scrollPosition;
   }
 }
